4cec7ead9b865fc67f9f4b519e036cd4815633ce
MohsinRaz3 <mohsinraz3@gmail.com>
2020-06-26 02:07:04 +0500

    FetchAPI GET/POST method

---
+++ index.js
@@ -0,0 +1,37 @@
+
+//Get data through Fetch API
+
+async function getData() {
+    let response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+    let data = await response.json();
+    console.log(data);
+    
+}
+getData();
+
+
+
+
+
+
+
+
+//Fetch API through POST method
+
+async function postData(){
+    let response = await fetch('https://jsonplaceholder.typicode.com/posts',{
+        method: "POST",
+        body: JSON.stringify({
+            username:"mohsinraz_",
+            password:333
+        }),
+        headers:{
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    });
+
+    let data = response.json();
+    console.log(data);
+    
+}
+postData();
