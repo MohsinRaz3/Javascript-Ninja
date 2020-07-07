a4a9d7c46f43ec1177d9f50be5660d36f52e6040
MohsinRaz3 <mohsinraz3@gmail.com>
2020-07-07 23:59:53 +0500

    update

--- index.js
+++ index.js
@@ -1,37 +1,28 @@
-
-//Get data through Fetch API
+//Get data through Fetch API convert into json file
 
 async function getData() {
-    let response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-    let data = await response.json();
-    console.log(data);
-    
+  let response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+  let data = await response.json();
+  console.log(data);
 }
 getData();
 
-
-
-
-
-
-
-
+//
 //Fetch API through POST method
 
-async function postData(){
-    let response = await fetch('https://jsonplaceholder.typicode.com/posts',{
-        method: "POST",
-        body: JSON.stringify({
-            username:"mohsinraz_",
-            password:333
-        }),
-        headers:{
-            "Content-type": "application/json; charset=UTF-8"
-        }
-    });
+async function postData() {
+  let response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: JSON.stringify({
+      username: "mohsinraz_",
+      password: 333,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  });
 
-    let data = response.json();
-    console.log(data);
-    
+  let data = response.json();
+  console.log(data);
 }
 postData();
